16f0d9c1fb9f925da89d1fefec3c0e6a706b4c2a
Indira <134701198+indirarara@users.noreply.github.com>
2024-10-10 09:53:43 +0800

    set up init

--- CalculusPy/__init__.py
+++ CalculusPy/__init__.py
@@ -1,5 +1,5 @@
 from grafik_fungsi.module_grafik import create_plot
-from kekontinuan.module_Kekontinuan import is_continuous, check_continuity_interval, check_continuity_at_points, is_continuous_everywhere
+from kekontinuan.module_kekontinuan import is_continuous, check_continuity_interval, check_continuity_at_points, is_continuous_everywhere
 from trigonometri.module_trigonometri import trigonometri, penyesualian_sudut_dengan_kuadran, hitung_trigonometri_dengan_kuadran
 from limit.module_limit import hitung_limit_kanan_kiri
 from domain_range.module_domain_dan_range import analisis_fungsi
@@ -16,3 +16,4 @@ __all__ = [
   'hitung_limit_kanan_kiri',
   'analisis_fungsi'
 ]
+
